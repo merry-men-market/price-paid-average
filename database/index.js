2a2aed2795c02a6feff35cd9531358a338dc92d0
Julius Simtim <juliussimtim@gmail.com>
2019-04-27 10:22:11 -0700

    Update confirmation for new laptop transfer.

--- database/index.js
+++ database/index.js
@@ -1,26 +1,22 @@
-const mongoose = require('mongoose');
-const Stock = require('./StockPricePaid/StockScheme.js');
 
-const mongoUri = 'mongodb://localhost/stock';
-// const mongoUri = 'mongodb://gary:abcd1234@ds031922.mlab.com:31922/front-end-capstone-project';
-// const mongoUri = process.env.DATABASEURL;
-mongoose.connect(mongoUri, { useNewUrlParser: true },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Connected to the database');
-    }
+require('../server/server.js');
+const Sequelize = require('sequelize');
+
+const user = 'root';
+const pass = '';
+
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('test', user, pass, {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
-const db = mongoose.connection;
-
-const getPaidPrice = (id, callback) => {
-  const query = { id };
-  Stock.find(query, (err, data) => {
-    if (err) callback(err);
-    callback(data);
-  });
-};
-
-module.exports = db;
-module.exports.getPaidPrice = getPaidPrice;
